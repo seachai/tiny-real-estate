645bedbdd0a738fda33b2840f1f1fe303f14b343
Chai Lee <imchai@gmail.com>
2019-05-30 21:37:42 -0400

    Working on register form

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -14,13 +14,37 @@ export default class Register extends Component {
         <div className="container">
           <h1>Register Account</h1>
           <div className="sign-up">
-          <form className="forms">
-            <label>Personal Info</label><br />
-            <input type="firstname" name="firstname" placeholder="First Name" /><br />
-            <input type="lastname" name="lastname" placeholder="Last Name" /><br />
-            <input type="text" name="email" placeholder="Email" /><br />
-            <button type="submit">Register</button>
-          </form>
+            <form className="forms">
+              <label>First Name *</label>
+              <br />
+              <input
+                type="firstname"
+                name="firstname"
+                placeholder="First Name"
+                className="firstName"
+              />
+              <br />
+              <label>Last Name *</label>
+              <br />
+              <input
+                type="lastname"
+                name="lastname"
+                placeholder="Last Name"
+                className="lastName"
+              />
+              <br />
+              <label>Email *</label>
+              <br />
+              <input
+                type="text"
+                name="email"
+                placeholder="Email"
+                className="email"
+                required
+              />
+              <br />
+              <button type="submit">Register</button>
+            </form>
           </div>
         </div>
       </section>
