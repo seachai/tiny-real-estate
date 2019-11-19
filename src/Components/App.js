d0b3bf2cc036b8d0d6a56a8dd357d8756537c7cf
Chai Lee <imchai@gmail.com>
2019-11-19 09:17:36 -0500

    Working on redux logic for store

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./utils/ScrollToTop";
 
-import { Provider } from "react-redux";
 import ListingData from "./data/listingsData";
 
 import Header from "./Header.js";
@@ -16,20 +15,18 @@ const App = () => {
   return (
     <Router>
       <ScrollToTop />
-      <Provider store={ListingData}>
-        <Header />
-        <section id="content-area">
-          <Switch>
-            <Route exact path="/" component={RealEstate} />
-            <Route path="/about" component={AboutUs} />
-            <Route exact path={ListingData.path} component={Info} />
-            {/* {ListingData.map(route => (
+      <Header />
+      <section id="content-area">
+        <Switch>
+          <Route exact path="/" component={RealEstate} />
+          <Route path="/about" component={AboutUs} />
+          <Route exact path={ListingData.path} component={Info} />
+          {/* {ListingData.map(route => (
             <Route path={route.path} component={Info} />
           ))} */}
-          </Switch>
-        </section>
-        <Footer />
-      </Provider>
+        </Switch>
+      </section>
+      <Footer />
     </Router>
   );
 };
